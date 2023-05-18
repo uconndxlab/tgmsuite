63a510f725d3eab01291ac8d7af55b60901c29db
Joel Salisbury <joel.saisbury@gmail.com>
2023-05-18 14:06:58 -0400

    fixing the empty script.

--- public/empty.php
+++ public/empty.php
@@ -4,7 +4,11 @@ $db = new SQLite3('turfgrass.db');
 
 // Step 2: Get a list of all tables in the database
 $tables = $db->query("SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence'");
-
+while($table = $tables->fetchArray(SQLITE3_ASSOC)) {
+    $db->exec('DELETE FROM ' . $table['name']);
+    echo "Table " . $table['name'] . " has been emptied.<br>";
+}
+die();
 // Step 3: Generate a SQL script to drop all the tables
 while ($table = $tables->fetchArray()) {
     $db->exec('DROP TABLE IF EXISTS ' . $table['name']);
